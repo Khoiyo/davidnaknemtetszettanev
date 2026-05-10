7c27d96cb513e88e16f4f07b5ab62222243a4122
Khoiyo <nagylacicsak1@gmail.com>
2026-05-10 21:29:32 +0200

    sajat video módosítva

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -2,7 +2,7 @@
 <div>
 <h2>Főoldal</h2>
 <p> Ez a oldal be mutatja a képek fel töltését  fő oldal videókkal  és képpel CRUD fájl kezeléssel és üzenet füllel kilépes gombbal.</p>
-<div class="media-box"><h3>Saját videó</h3><video class="responsive" controls><source src="images/intro.mp4" type="video/mp4">A böngésző nem támogatja a videót.</video></div>
+<div class="media-box"><h3>Saját videó</h3><video class="responsive" controls><source src="images/random_sajat_video.mp4" type="video/mp4">A böngésző nem támogatja a videót.</video></div>
 <div class="youtube"><h3>YouTube videó</h3><iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube videó" allowfullscreen></iframe></div>
 </div>
 <div><h3>Kép</h3><img class="responsive" src="images/arc.gif" alt="Bemutató kép"><div class="map"><h3>Telephely térkép</h3><iframe src="https://www.google.com/maps?q=Miskolc&output=embed" loading="lazy" title="Google térkép"></iframe></div></div>
