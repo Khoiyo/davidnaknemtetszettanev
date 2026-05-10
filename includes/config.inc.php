0f54c9d1a55ec69f9665509fd7f9614e4794452f
Khoiyo <nagylacicsak1@gmail.com>
2026-05-10 21:01:37 +0200

    adatbazis kapcsolatok létrehozva  + design

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,28 +1,26 @@
 <?php
-$ablakcim = array(
-    'cim' => 'Mini honlap Kft.',
-);
-
-$fejlec = array(
+$ablakcim = ['cim' => 'Web Progzamozás Gyakorlat Beadandó'];
+$fejlec = [
     'kepforras' => 'logo.png',
-    'kepalt' => 'logo',
-	'cim' => 'Mini honlap',
-	'motto' => ''
-);
+    'kepalt' => 'Honlap logó',
+    'cim' => 'Honlap',
+    'motto' => 'Web Progzamozás Gyakorlat Beadandó'
+];
+$lablec = [
+    'copyright' => 'Copyright ' . date('Y') . '.',
+    'ceg' => 'Nagy László - I8EP12 | Szegletes Dávid - SZPBUQ'
+];
 
-$lablec = array(
-    'copyright' => 'Copyright '.date("Y").'.',
-    'ceg' => 'Mini honlap Kft.'
-);
-
-$oldalak = array(
-	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap', 'menun' => array(1,1)),
-	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
-    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
-    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
-    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
-    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
-);
-
-$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+$oldalak = [
+    '/' => ['fajl' => 'cimlap', 'szoveg' => 'Főoldal', 'menun' => [1, 1]],
+    'kepek' => ['fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => [1, 1]],
+    'kapcsolat' => ['fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => [1, 1]],
+    'crud' => ['fajl' => 'crud', 'szoveg' => 'CRUD', 'menun' => [1, 1]],
+    'uzenetek' => ['fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => [0, 1]],
+    'belepes' => ['fajl' => 'belepes', 'szoveg' => 'Bejelentkezés', 'menun' => [1, 0]],
+    'kilepes' => ['fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => [0, 1]],
+    'belep' => ['fajl' => 'belep', 'szoveg' => '', 'menun' => [0, 0]],
+    'regisztral' => ['fajl' => 'regisztral', 'szoveg' => '', 'menun' => [0, 0]],
+];
+$hiba_oldal = ['fajl' => '404', 'szoveg' => 'A keresett oldal nem található!'];
 ?>
--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,21 @@
 <?php
-    try {
-    $conn = new PDO('mysql:host=localhost;dbname=nagylaszlo;charset=utf8',
-            'nagylaszlo',
-            'Admin12345',
-	        	[
-                	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-            	]
-	);
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gyakorlat7');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+try {
+    $conn = new PDO(
+        'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
+        DB_USER,
+        DB_PASS,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ]
+    );
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    die('Adatbázis kapcsolódási hiba: ' . htmlspecialchars($e->getMessage()));
 }
 ?>
