119c9e4e71095c7ffb0036b51f88508a00e6a296
Khoiyo <nagylacicsak1@gmail.com>
2026-05-10 21:12:45 +0200

    oldal html fájl kész

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,43 +1,43 @@
-<?php session_start(); ?>
-<?php if(file_exists('./logicals/'.$keres['fajl'].'.php')) { include("./logicals/{$keres['fajl']}.php"); } ?>
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="hu">
 <head>
-	<meta charset="utf-8">
-	<title><?= $ablakcim['cim'] . ( (isset($ablakcim['mottó'])) ? ('|' . $ablakcim['mottó']) : '' ) ?></title>
-	<link rel="stylesheet" href="./styles/stilus.css" type="text/css">
-	<?php if(file_exists('./styles/'.$keres['fajl'].'.css')) { ?><link rel="stylesheet" href="./styles/<?= $keres['fajl']?>.css" type="text/css"><?php } ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= htmlspecialchars($ablakcim['cim']) ?></title>
+    <link rel="stylesheet" href="styles/stilus.css">
+    <link rel="stylesheet" href="styles/tablazat.css">
 </head>
 <body>
-	<header>
-		<img src="./images/<?=$fejlec['kepforras']?>" alt="<?=$fejlec['kepalt']?>">
-		<h1><?= $fejlec['cim'] ?></h1>
-		<?php if (isset($fejlec['motto'])) { ?><h2><?= $fejlec['motto'] ?></h2><?php } ?>
-		<?php if(isset($_SESSION['login'])) { ?>Bejlentkezve: <strong><?= $_SESSION['csn']." ".$_SESSION['un']." (".$_SESSION['login'].")" ?></strong><?php } ?>
-	</header>
-    <div id="wrapper">
-        <aside id="nav">
-            <nav>
-                <ul>
-					<?php foreach ($oldalak as $url => $oldal) { ?>
-						<?php if(! isset($_SESSION['login']) && $oldal['menun'][0] || isset($_SESSION['login']) && $oldal['menun'][1]) { ?>
-							<li<?= (($oldal == $keres) ? ' class="active"' : '') ?>>
-							<a href="<?= ($url == '/') ? '.' : $url ?>">
-							<?= $oldal['szoveg'] ?></a>
-							</li>
-						<?php } ?>
-					<?php } ?>
-                </ul>
-            </nav>
-        </aside>
-        <div id="content">
-            <?php include("./templates/pages/{$keres['fajl']}.tpl.php"); ?>
-        </div>
-    </div>
-    <footer>
-        <?php if(isset($lablec['copyright'])) { ?>&copy;&nbsp;<?= $lablec['copyright'] ?> <?php } ?>
-		&nbsp;
-        <?php if(isset($lablec['ceg'])) { ?><?= $lablec['ceg']; ?><?php } ?>
-    </footer>
+<header>
+    <img src="images/<?= htmlspecialchars($fejlec['kepforras']) ?>" alt="<?= htmlspecialchars($fejlec['kepalt']) ?>">
+    <h1><?= htmlspecialchars($fejlec['cim']) ?></h1>
+    <p><?= htmlspecialchars($fejlec['motto']) ?></p>
+    <?php if (isset($_SESSION['login'])): ?>
+        <p class="login-info">Bejelentkezett: <?= htmlspecialchars($_SESSION['csn'] . ' ' . $_SESSION['un'] . ' (' . $_SESSION['login'] . ')') ?></p>
+    <?php endif; ?>
+</header>
+<div id="wrapper">
+    <aside>
+        <nav aria-label="Főmenü">
+            <ul>
+                <?php foreach ($oldalak as $url => $oldalAdat): ?>
+                    <?php $menuIndex = isset($_SESSION['login']) ? 1 : 0; ?>
+                    <?php if ($oldalAdat['szoveg'] !== '' && $oldalAdat['menun'][$menuIndex]): ?>
+                        <li class="<?= ($keres['fajl'] === $oldalAdat['fajl']) ? 'active' : '' ?>">
+                            <a href="<?= ($url === '/') ? '.' : htmlspecialchars($url) ?>"><?= htmlspecialchars($oldalAdat['szoveg']) ?></a>
+                        </li>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </ul>
+        </nav>
+    </aside>
+    <main id="content">
+        <?php include("./templates/pages/{$keres['fajl']}.tpl.php"); ?>
+    </main>
+</div>
+<footer>
+    <p>&copy; <?= htmlspecialchars($lablec['copyright']) ?> <?= htmlspecialchars($lablec['ceg']) ?></p>
+</footer>
+<script src="scripts.js"></script>
 </body>
 </html>
