28ffed01846a14c3efb3e525a5f07bf72b9c7fa9
Igazi1 <szegletesdavid@gmail.com>
2026-05-10 21:13:10 +0200

    belépes html kész

--- templates/pages/belepes.tpl.php
+++ templates/pages/belepes.tpl.php
@@ -1,23 +1,14 @@
-    <form action = "belep" method = "post">
-      <fieldset>
-        <legend>Bejlentkezés</legend>
-        <br>
-        <input type="text" name="felhasznalo" placeholder="felhasználó" required><br><br>
-        <input type="password" name="jelszo" placeholder="jelszó" required><br><br>
-        <input type="submit" name="belepes" value="Belépés">
-        <br>&nbsp;
-      </fieldset>
-    </form>
-    <h3>Regisztrálja magát, ha még nem felhasználó!</h2>
-    <form action = "regisztral" method = "post">
-      <fieldset>
-        <legend>Regisztráció</legend>
-        <br>
-        <input type="text" name="vezeteknev" placeholder="vezetéknév" required><br><br>
-        <input type="text" name="utonev" placeholder="utónév" required><br><br>
-        <input type="text" name="felhasznalo" placeholder="felhasználói név" required><br><br>
-        <input type="password" name="jelszo" placeholder="jelszó" required><br><br>
-        <input type="submit" name="regisztracio" value="Regisztráció">
-        <br>&nbsp;
-      </fieldset>
-    </form>
+<h2>Bejelentkezés</h2>
+<form method="post" action="belep">
+<div class="form-row"><label for="felhasznalo">Felhasználónév</label><input id="felhasznalo" name="felhasznalo" required></div>
+<div class="form-row"><label for="jelszo">Jelszó</label><input id="jelszo" name="jelszo" type="password" required></div>
+<button type="submit">Belépés</button>
+</form>
+<h3>Regisztráció</h3>
+<form method="post" action="regisztral">
+<div class="form-row"><label>Vezetéknév</label><input name="vezeteknev" required></div>
+<div class="form-row"><label>Utónév</label><input name="utonev" required></div>
+<div class="form-row"><label>Felhasználónév</label><input name="felhasznalo" maxlength="12" required></div>
+<div class="form-row"><label>Jelszó</label><input name="jelszo" type="password" required></div>
+<button type="submit">Regisztrálok</button>
+</form>
