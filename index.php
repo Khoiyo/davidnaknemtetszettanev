add328e26caf0ba489ef7f363bf6a9b980dc8af0
Igazi1 <szegletesdavid@gmail.com>
2026-05-10 21:05:00 +0200

    index php file fent

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
-	include('./includes/config.inc.php');
-	$oldal = $_SERVER['QUERY_STRING'];
-	if ($oldal!="") {
-		if (isset($oldalak[$oldal]) && file_exists("./templates/pages/{$oldalak[$oldal]['fajl']}.tpl.php")) {
-			$keres = $oldalak[$oldal];
-		}
-		else { 
-			$keres = $hiba_oldal;
-			header("HTTP/1.0 404 Not Found");
-		}
-	}
-	else $keres = $oldalak['/'];
-	include('./templates/index.tpl.php'); 
+session_start();
+include('./includes/config.inc.php');
+include('./includes/db.php');
+
+$oldal = trim($_SERVER['QUERY_STRING'] ?? '', '/');
+if ($oldal === '') { $oldal = '/'; }
+
+if (isset($oldalak[$oldal]) && file_exists("./templates/pages/{$oldalak[$oldal]['fajl']}.tpl.php")) {
+    $keres = $oldalak[$oldal];
+} else {
+    $keres = $hiba_oldal;
+    header('HTTP/1.0 404 Not Found');
+}
+
+$logical = "./logicals/{$keres['fajl']}.php";
+if (file_exists($logical)) { include($logical); }
+include('./templates/index.tpl.php');
 ?>
