d3ee7ffdc6f5a31238c6eff7d18e12d5334174a6
Khoiyo <nagylacicsak1@gmail.com>
2026-05-10 21:39:04 +0200

    kép lecserélve

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -5,5 +5,5 @@
 <div class="media-box"><h3>Saját videó</h3><video class="responsive" controls><source src="images/random_sajat_video.mp4" type="video/mp4">A böngésző nem támogatja a videót.</video></div>
 <div class="youtube"><h3>YouTube videó</h3><iframe src="https://www.youtube.com/embed/m52ljs78S24?rel=0" title="YouTube videó" allowfullscreen></iframe></div>
 </div>
-<div><h3>Kép</h3><img class="responsive" src="images/arc.gif" alt="Bemutató kép"><div class="map"><h3>Telephely térkép</h3><iframe src="https://www.google.com/maps?q=Miskolc&output=embed" loading="lazy" title="Google térkép"></iframe></div></div>
+<div><h3>Kép</h3><img class="responsive" src="images/githubcica.png" alt="Bemutató kép"><div class="map"><h3>Telephely térkép</h3><iframe src="https://www.google.com/maps?q=Miskolc&output=embed" loading="lazy" title="Google térkép"></iframe></div></div>
 </section>
